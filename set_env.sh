90d4931df6f1372b193e28c0fca88bc8bc5bc012
Eduardo <eduardo.barros@outlook.com>
2020-12-06 15:55:46 -0300

    fisrt commit

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagram-database.cf5dz7dd5fqa.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test1-161405308651-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
+bash
